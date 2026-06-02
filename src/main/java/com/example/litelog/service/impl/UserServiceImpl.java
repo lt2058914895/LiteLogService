fc99ede4e78801081f2f8aeed9fbd98a8ffcb727
liting <2058914895@qq.com>
2026-06-02 15:33:40 +0800

    fix

--- src/main/java/com/example/litelog/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/litelog/service/impl/UserServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class erviceImpl implements UserService {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
